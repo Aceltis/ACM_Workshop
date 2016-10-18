57a5600effa7931335d537c2de24044981e524d1
Le Cahain, Amandine <amandine.lecahain@ea.com>
2016-10-18 12:19:21 +0200

    project setup

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Hello from "./Hello";
+
+var root: HTMLElement = document.getElementById("root");
+
+ReactDOM.render(<Hello name="Amandine" />, root);
