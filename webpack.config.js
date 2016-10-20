9f5f360445140b9b54b488807632db41a17ea091
Le Cahain, Amandine <aceltis.ig@gmail.com>
2016-10-20 11:52:38 +0200

    Configure react hot module replacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,41 @@
 var path = require("path");
+var webpack = require('webpack');
 
-var config = {
-    /*
-     * app.ts represents the entry point to your web application. Webpack will
-     * recursively go through every "require" statement in app.ts and
-     * efficiently build out the application's dependency tree.
-     */
-    entry: ["./src/app.tsx"],
+module.exports = {
+    devtool: 'source-map',
+    debug: true,
+    cache: true,
 
-    /*
-     * The combination of path and filename tells Webpack what name to give to
-     * the final bundled JavaScript file and where to store this file.
-     */
+    entry: [
+        'webpack-dev-server/client?http://0.0.0.0:3000',
+        'webpack/hot/only-dev-server',
+        "./src/app"
+    ],
+
     output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "bundle.js"
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: '/static/'
     },
 
-    /*
-     * resolve lets Webpack now in advance what file extensions you plan on
-     * "require"ing into the web application, and allows you to drop them
-     * in your code.
-     */
     resolve: {
         extensions: ["", ".ts", ".tsx", ".js"]
     },
 
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
     module: {
-        /*
-         * Each loader needs an associated Regex test that goes through each
-         * of the files you've included (or in this case, all files but the
-         * ones in the excluded directories) and finds all files that pass
-         * the test. Then it will apply the loader to that file. I haven't
-         * installed ts-loader yet, but will do that shortly.
-         */
         loaders: [
             {
                 test: /\.tsx?$/,
-                loader: "ts-loader",
-                exclude: /node_modules/
+                loaders: [
+                    'react-hot',
+                    'ts-loader'
+                ],
+                include: path.join(__dirname, 'src')
             }
         ]
     }
 };
-
-module.exports = config;
